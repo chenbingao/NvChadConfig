6500397374b8fd27e0eeca33cfabe9c43875b920
shiro <shiro@shirodeiMac.local>
2024-12-13 18:30:53 -0600

    feat: first commit

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -5,6 +5,12 @@ require "nvchad.mappings"
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
+map("n", "<leader>fs", "<cmd> w <cr>", { desc = "file save" })
+
 map("i", "jk", "<ESC>")
+
+-- local nomap = vim.keymap.del
+
+-- nomap({ "n", "i", "v" }, "<C-s>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
