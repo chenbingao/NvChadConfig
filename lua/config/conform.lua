2d260e8464782b71985ed516b04944ff5a48272c
Shiro <615149633@qq.com>
2025-02-13 10:00:41 -0600

    staging

--- lua/config/conform.lua
+++ lua/config/conform.lua
@@ -19,9 +19,9 @@ require("conform").setup {
   },
 }
 
-vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = "*",
-  callback = function(args)
-    require("conform").format { bufnr = args.buf }
-  end,
-})
+-- vim.api.nvim_create_autocmd("BufWritePre", {
+--   pattern = "*",
+--   callback = function(args)
+--     require("conform").format { bufnr = args.buf }
+--   end,
+-- })
